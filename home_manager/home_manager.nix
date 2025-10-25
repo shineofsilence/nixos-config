42cdf65561a33f695deb292dbd3c4cc4eeb152fd
KayRos NewSky <order.exterminatus@yandex.ru>
2025-10-25 22:44:34 +0300

    Hyprland

--- home_manager/home_manager.nix
+++ home_manager/home_manager.nix
@@ -7,7 +7,15 @@
   # Импортируем конфигурации приложений
   imports = [
     ./console/git.nix
-    #./console/zsh.nix
+    ./console/zsh.nix
     # ./console/neovim.nix
+    
+    # Конфигурация Hyprland
+    (import ../../wm/hyprland.nix)
   ];
+  
+  # Копируем конфигурацию Hyprland в домашнюю директорию
+  home.file = {
+    ".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
+  };
 }
