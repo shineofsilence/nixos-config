866f7a62b3c14eed96b59d5da54ab56a301cde26
KayRos NewSky <order.exterminatus@yandex.ru>
2025-10-28 05:12:42 +0300

    Hyprland install

--- home_manager/hyprland.nix
+++ home_manager/hyprland.nix
@@ -4,5 +4,20 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+    extraConfig = ''
+      # Минимальная рабочая конфигурация
+      exec = systemctl --user import-environment
+      exec = dbus-update-activation-environment --systemd
+
+      monitor = ,preferred,auto,1
+
+      input {
+        kb_layout = us,ru
+        kb_options = grp:alt_shift_toggle
+      }
+
+      bind = ALT, F1, exec, firefox
+      bind = ALT, RETURN, exec, kitty
+    '';
   };
 }
