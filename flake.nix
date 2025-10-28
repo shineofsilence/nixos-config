4714a9b2a04681e711da86bdba6e6e6665f05165
KayRos NewSky <order.exterminatus@yandex.ru>
2025-10-28 04:57:48 +0300

    Hyprland install

--- flake.nix
+++ flake.nix
@@ -20,7 +20,16 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.kayros = import ./home_manager/default.nix;
+            home-manager.users.kayros = {
+				imports = [
+							./home_manager/zsh.nix
+							./home_manager/git.nix
+							./home_manager/hyprland.nix
+							];
+				home.username = "kayros";
+				home.homeDirectory = "/home/kayros";
+				home.stateVersion = "25.05";
+			};
           }
         ];
       };
